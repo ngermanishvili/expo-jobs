3f0271a30464ca0d62ff9a0fb43eea7d48f7d770
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-08-24 01:32:36 +0400

    🚀 added component structure

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Alo Kokiii shena xaR??
+            </Text>
+
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default Home;
